5f3a109b402649d58dbb5c3269351fdb433279e2
Tarun9875 <tarunpatel512005@gmail.com>
2026-03-30 22:07:08 +0530

    Update cors.ts

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -2,7 +2,7 @@
 import cors from "cors";
 
 const corsOptions = {
-  origin: "http://localhost:5173", // 👈 frontend URL
+  origin: "https://moviebook-u80e.onrender.com", // 👈 frontend URL
   credentials: true,               // 👈 allow cookies / auth headers
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
